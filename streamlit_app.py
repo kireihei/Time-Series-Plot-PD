5ef6c472cb2e526a8b0ddbe0fb6f4f6433fbb6f2
kireihei <47026893+kireihei@users.noreply.github.com>
2023-02-12 13:17:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-If you have any questions, please refer to {documentation}(https://docs.streamlit.io/).
+If you have any questions, please refer to [documentation](https://docs.streamlit.io/).
 """
 
 # load data file
