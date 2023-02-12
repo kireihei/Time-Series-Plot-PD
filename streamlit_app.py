3f8cd9ecf10ebf892cb2351acdfb04e644404826
kireihei <47026893+kireihei@users.noreply.github.com>
2023-02-12 13:06:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,32 +28,12 @@ df = load_data()
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
 
-# select box
-company_ID = st.selectbox('Select a Company ID',
-                          options=list(df.loc[:, "CompanyID"].unique()))
-st.write('The selected company is', company_ID)
-
-# line chart
-selected_df = df[df.loc[:, "CompanyID"] == company_ID]
-st.line_chart(selected_df, x="DataDate", y="PD")
-
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    # select box
+    company_ID = st.selectbox('Select a Company ID',
+                              options=list(df.loc[:, "CompanyID"].unique()))
+    st.write('The selected company is', company_ID)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    # line chart
+    selected_df = df[df.loc[:, "CompanyID"] == company_ID]
+    st.line_chart(selected_df, x="DataDate", y="PD")
